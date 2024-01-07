4b0bb2409fb4fca5f9060e3a6d62be8df163921e
dangkhoabo2002 <dangkhoabo2002@gmail.com>
2024-01-07 12:58:53 +0700

    Homepage

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="py-6 text-center border-t-2 border-stone-400">
+      Copyright © 2024 All rights reserved | Powered by Master Help Desk
+    </div>
+  );
+}
---
+++ src/components/Navigation.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../logo.png";
+
+export default function Navigation() {
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center px-20 py-4">
+        <Link to={`/`}>
+          <div className=" flex flex-row items-center gap-x-4">
+            <img className="object-cover h-18 w-16" src={Logo} alt="logo" />
+            <div className="text-3xl font-semibold tracking-wide">
+              Master Help Desk
+            </div>
+          </div>
+        </Link>
+        <div className="flex flex-row justify-between items-center gap-x-10 ">
+          <Link to={`/contact`}>
+            <p>
+              <button className="font-semibold">About Us</button>
+            </p>
+          </Link>
+          <Link to={`/`}>
+            <p>
+              <button className="font-semibold">Feature News</button>
+            </p>
+          </Link>
+          <Link to={`/`}>
+            <p>
+              <button className="font-semibold">Solutions</button>
+            </p>
+          </Link>
+          <button class="bg-[#3867A5] hover:bg-blue-700 text-white  py-2 px-6 rounded-full">
+            Create MHD account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
