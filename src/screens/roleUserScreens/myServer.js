d2e3c69a7388e9b02e80fb7cfd1f8ea7899d0378
khang20112002 <khangdcse161156@fpt.edu.vn>
2024-03-11 11:09:44 +0700

    add org modal 50%

--- src/screens/roleUserScreens/myServer.js
+++ src/screens/roleUserScreens/myServer.js
@@ -3,7 +3,7 @@ import { RiServerFill } from "react-icons/ri";
 import Empty from "../../images/empty.png";
 import Footer from "../../components/userFooter";
 import ButtonAddServer from "../buttonAddServer";
-import ApartmentIcon from '@mui/icons-material/Apartment';
+import ApartmentIcon from "@mui/icons-material/Apartment";
 import {
   Grid,
   Checkbox,
@@ -12,28 +12,48 @@ import {
   FormControlLabel,
   Box,
   Item,
+  Typography,
+  Modal,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  FormHelperText,
 } from "@mui/material";
-import Stack from '@mui/material/Stack';
-import { purple } from '@mui/material/colors';
-import { styled } from '@mui/material/styles';
-
-
+import Stack from "@mui/material/Stack";
+import { purple } from "@mui/material/colors";
+import { styled } from "@mui/material/styles";
 
 export default function LandingPage() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(!open);
 
+  const [addOrg, setAddOrg] = React.useState(false);
+  const handleAdd = () => setAddOrg(!addOrg); // Toggle addOrg state
+  const handleClose = () => setAddOrg(false); // Close modal
+
   const ColorButton = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText(purple[500]),
     backgroundColor: "#E6E6E6",
-    color:"#637381",
+    color: "#637381",
     fontSize: "24px",
     width: "100%",
     height: "200px",
-    '&:hover': {
+    "&:hover": {
       backgroundColor: "#D2D2D2",
     },
   }));
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 900,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
     <div>
@@ -51,7 +71,7 @@ export default function LandingPage() {
             </p>
           </div>
         </div>
-{/* 
+        {/* 
         <div class="mb-3">
           <div class="relative mb-4 flex w-full flex-wrap items-stretch">
             <input
@@ -81,8 +101,124 @@ export default function LandingPage() {
             </span>
           </div>
         </div> */}
-        <div className="mt-3" style={{}}>
-        <ColorButton variant="contained">+</ColorButton>
+        <div className="mt-3">
+          <ColorButton onClick={handleAdd} variant="contained">
+            +
+          </ColorButton>
+
+          <Modal
+            keepMounted
+            open={addOrg}
+            onClose={handleClose}
+            aria-labelledby="keep-mounted-modal-title"
+            aria-describedby="keep-mounted-modal-description"
+          >
+            <Box sx={style}>
+              <div className=" pb-2  text-center border-b-2 border-stone-200">
+                <div className="header flex flex-row items-center gap-x-3">
+                  <p
+                    className="font-semibold"
+                    style={{ fontSize: "28px", color: "#637381" }}
+                  >
+                    ADD ORGANIZATON
+                  </p>
+                </div>
+              </div>
+
+              <Grid container alignItems="center" spacing={2} mt={1}>
+                <Grid item>
+                  <Typography
+                    className="mt-3"
+                    style={{
+                      fontSize: "16px",
+                      fontWeight: "400",
+                    }}
+                  >
+                    Organization name:
+                  </Typography>
+                </Grid>
+                <Grid item xs>
+                  <FormControl fullWidth variant="outlined">
+                    <OutlinedInput
+                      inputProps={{
+                        "aria-label": "Organization name",
+                      }}
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+
+              <Grid container alignItems="center" spacing={2} mt={1}>
+                <Grid item>
+                  <Typography
+                    className="mt-3"
+                    style={{
+                      fontSize: "16px",
+                      fontWeight: "400",
+                    }}
+                  >
+                    Phone number:
+                  </Typography>
+                </Grid>
+                <Grid item xs>
+                  <FormControl fullWidth variant="outlined">
+                    <OutlinedInput
+                      inputProps={{
+                        "aria-label": "Phone number",
+                      }}
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+
+              <Grid container alignItems="center" spacing={2} mt={1}>
+                <Grid item>
+                  <Typography
+                    className="mt-3"
+                    style={{
+                      fontSize: "16px",
+                      fontWeight: "400",
+                    }}
+                  >
+                    Email:
+                  </Typography>
+                </Grid>
+                <Grid item xs>
+                  <FormControl fullWidth variant="outlined">
+                    <OutlinedInput
+                      inputProps={{
+                        "aria-label": "Email",
+                      }}
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+
+              <Grid container alignItems="center" spacing={2} mt={1}>
+                <Grid item>
+                  <Typography
+                    className="mt-3"
+                    style={{
+                      fontSize: "16px",
+                      fontWeight: "400",
+                    }}
+                  >
+                    Add member:
+                  </Typography>
+                </Grid>
+                <Grid item xs>
+                  <FormControl fullWidth variant="outlined">
+                    <OutlinedInput
+                      inputProps={{
+                        "aria-label": "Member",
+                      }}
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+
+            </Box>
+          </Modal>
         </div>
 
         {/* <div className="flex flex-row">
