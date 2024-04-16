86f4dc535ba39d3cd02776af94f2e21b28bda9ec
khang20112002 <khangdcse161156@fpt.edu.vn>
2024-04-17 03:43:26 +0700

    Change Password (UI Only!)

--- src/screens/userProfile.js
+++ src/screens/userProfile.js
@@ -1,35 +1,52 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SidebarUser from "../components/sidebarUser";
 import { useNavigate } from "react-router-dom";
-
 import NavigationUser from "../components/navUserProfile";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-
 import InputAdornment from "@mui/material/InputAdornment";
 import PersonIcon from "@mui/icons-material/Person";
 import EmailIcon from "@mui/icons-material/Email";
 import LockResetIcon from "@mui/icons-material/LockReset";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import { MuiOtpInput } from "mui-one-time-password-input";
+import Box from "@mui/material/Box";
+import { Alert, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { motion } from "framer-motion";
 
 import "../css/userProfile.css";
 
 export default function UserProfile() {
+  const navigate = useNavigate();
+  const [fullNameData, setFullNameData] = useState({ full_name: "" });
+  const [userProfile, setUserProfile] = useState();
   const [isDisabled, setIsDisabled] = useState(true);
   const [showResetButton, setShowResetButton] = useState(false);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isOtpVerified, setIsOtpVerified] = useState(false);
+  const [showResetPasswordAlert, setShowResetPasswordAlert] = useState(false);
+  const [showOtpDialog, setShowOtpDialog] = useState(false);
+  const [showPasswordDialog, setShowPasswordDialog] = useState(false);
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
 
-  const navigate = useNavigate();
-  const [fullNameData, setData] = useState({
-    full_name: "",
-  });
-
-  const [userProfile, setUserProfile] = useState();
+  useEffect(() => {
+    handleGetUserProfile();
+  }, []);
 
   const handleChangeInput = (prop) => (event) => {
-    setData({ ...fullNameData, [prop]: event.target.value });
+    setFullNameData({ ...fullNameData, [prop]: event.target.value });
   };
 
-  // Đổi information của Profile
+  const handleEditClick = () => {
+    setIsDisabled(!isDisabled);
+    setShowResetButton(isDisabled);
+  };
+
   const handleUpdate = async () => {
     const updUrl = "http://127.0.0.1:5000/auth/update_information";
     const token = localStorage.getItem("access_token");
@@ -57,12 +74,9 @@ export default function UserProfile() {
       }
     } catch (error) {
       console.error("Error:", error);
-    } finally {
     }
   };
-  console.log("hello", fullNameData.full_name);
 
-  // Lấy information của user từ API
   const handleGetUserProfile = async () => {
     const getUrl = `http://127.0.0.1:5000/auth/get_profile`;
     const token = localStorage.getItem("access_token");
@@ -84,17 +98,7 @@ export default function UserProfile() {
       }
     } catch (error) {
       console.error("Error:", error);
-    } finally {
     }
-  };
-
-  console.log(userProfile);
-
-  // Điền thông tin vào Input
-
-  const handleEditClick = () => {
-    setIsDisabled(!isDisabled);
-    setShowResetButton(isDisabled);
   };
 
   const handleClickUpdate = () => {
@@ -103,28 +107,45 @@ export default function UserProfile() {
   };
 
   const handleResetClick = () => {
-    navigate(`/login/forgotPassword`);
+    setShowResetPasswordAlert(true);
+    setShowOtpDialog(true);
   };
 
-  useEffect(() => {
-    handleGetUserProfile();
-  }, []);
+  const handleCloseResetPasswordAlert = () => {
+    setShowResetPasswordAlert(false);
+  };
+
+  const handleCloseOtpDialog = () => {
+    setShowOtpDialog(false);
+  };
+
+  const handleOtpVerification = () => {
+    setIsOtpVerified(true);
+    setShowOtpDialog(false);
+    setShowPasswordDialog(true);
+  };
+
+  const handleChangePassword = (newPassword) => {
+    setShowPasswordDialog(false);
+  };
+
+  const handleChangePasswordFormSubmit = () => {
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    handleChangePassword(password);
+    setShowSuccessAlert(true);
+  };
 
   return (
-    <div className="">
-      {/*-------------- Navigation + Backgroud---------------- */}
-
+    <div className="" style={{ height: "100vh" }}>
       <NavigationUser />
-
-      {/*-------------- END OF Navigation + Backgroud---------------- */}
-
-      {/*-------------- LayoutBody ---------------- */}
-
       <div
         style={{
           display: "grid",
           gridTemplateColumns: "0fr 3fr",
-          height: "52vh",
+          height: "59vh",
         }}
       >
         <div
@@ -135,107 +156,274 @@ export default function UserProfile() {
         >
           <SidebarUser />
         </div>
-          <div className="profileField  flex flex-col gap-10 px-20 py-10 bg-[#F3F8FF]">
-            <div className="profileField  flex flex-row justify-start gap-72">
-              <div className="username">
-                <h1>Full Name</h1>
-
-                <TextField
-                  className="bg-[white]"
-                  disabled={isDisabled}
-                  id="outlined-basic"
-                  onChange={handleChangeInput("full_name")}
-                  placeholder={userProfile?.full_name}
-                  value={fullNameData?.full_name || userProfile?.full_name}
-                  size="small"
-                  sx={{ width: "auto" }}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start" className="pr-2">
-                        <PersonIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </div>
-
-              <div className="username">
-                <h1>Username</h1>
-
-                <TextField
-                  className="bg-[white]"
-                  disabled
-                  id="outlined-basic"
-                  // placeholder={userProfile.username}
-                  value={userProfile?.username}
-                  size="small"
-                  sx={{ width: "auto" }}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start" className="pr-2">
-                        <PersonIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </div>
+        <div className="profileField  flex flex-col gap-10 px-20 py-10 bg-[#F3F8FF]">
+          <div className="profileField  flex flex-row justify-start gap-72">
+            <div className="username">
+              <h1>Full Name</h1>
+              <TextField
+                className="bg-[white]"
+                disabled={isDisabled}
+                id="outlined-basic"
+                onChange={handleChangeInput("full_name")}
+                placeholder={userProfile?.full_name}
+                value={fullNameData?.full_name || userProfile?.full_name}
+                size="small"
+                sx={{ width: "auto" }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start" className="pr-2">
+                      <PersonIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </div>
-            <div className="flex flex-row gap-40 ">
-              <div className="email">
-                <h1>Email</h1>
-
-                <TextField
-                  className="bg-[white]"
-                  disabled
-                  id="outlined-basic"
-                  // placeholder={userProfile.email}
-                  value={userProfile?.email}
-                  size="small"
-                  sx={{ width: "400px" }}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start" className="pr-2">
-                        <EmailIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </div>
-            </div>
-            <div className="flex gap-10">
-              {!showResetButton && (
-                <Button variant="outlined" onClick={handleEditClick}>
-                  Edit Profile
-                </Button>
-              )}
-
-              {showResetButton && (
-                <>
-                  <Button variant="outlined" onClick={handleClickUpdate}>
-                    Save Changes
-                  </Button>
-                  <Button
-                    size="small"
-                    variant="contained"
-                    onClick={handleResetClick}
-                  >
-                    <LockResetIcon />
-                    <span className="px-2">Change Password</span>
-                  </Button>
-                  <Button
-                    size="medium"
-                    variant="text"
-                    onClick={handleEditClick}
-                  >
-                    <span className="">Cancel</span>
-                  </Button>
-                </>
-              )}
+            <div className="username">
+              <h1>Username</h1>
+              <TextField
+                className="bg-[white]"
+                disabled
+                id="outlined-basic"
+                value={userProfile?.username}
+                size="small"
+                sx={{ width: "auto" }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start" className="pr-2">
+                      <PersonIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </div>
           </div>
-        </div>
+          <div className="flex flex-row gap-40 ">
+            <div className="email">
+              <h1>Email</h1>
+              <TextField
+                className="bg-[white]"
+                disabled
+                id="outlined-basic"
+                value={userProfile?.email}
+                size="small"
+                sx={{ width: "400px" }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start" className="pr-2">
+                      <EmailIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+          </div>
+          <div className="flex gap-10">
+            {!showResetButton && (
+              <Button variant="outlined" onClick={handleEditClick}>
+                Edit Profile
+              </Button>
+            )}
+            {showResetButton && (
+              <>
+                <Button variant="outlined" onClick={handleClickUpdate}>
+                  Save Changes
+                </Button>
+                <Button
+                  size="small"
+                  variant="contained"
+                  onClick={handleResetClick}
+                >
+                  <LockResetIcon />
+                  <span className="px-2">Change Password</span>
+                </Button>
+                <Button size="medium" variant="text" onClick={handleEditClick}>
+                  <span className="">Cancel</span>
+                </Button>
+              </>
+            )}
+          </div>
 
-      {/*-------------- END OF LayoutBody ---------------- */}
+          {showSuccessAlert && (
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 50 }}
+              transition={{ duration: 0.5 }}
+              style={{
+                position: "fixed",
+                bottom: "20px",
+                right: "20px",
+                zIndex: 9999,
+              }}
+            >
+              <Alert
+                open={true}
+                onClose={() => setShowSuccessAlert(false)}
+                severity="success"
+                action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => setShowSuccessAlert(false)}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                }
+              >
+                Password changed successfully.
+              </Alert>
+            </motion.div>
+          )}
+        </div>
+      </div>
+
+      {/* Thông báo gửi OTP */}
+      {showResetPasswordAlert && (
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 50 }}
+          transition={{ duration: 0.5 }}
+          style={{
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+            zIndex: 9999,
+          }}
+        >
+          <Alert
+            open={true}
+            onClose={handleCloseResetPasswordAlert}
+            severity="info"
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={handleCloseResetPasswordAlert}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+          >
+            An email with the OTP has been sent to your registered email
+            address. Please check your email to continue.
+          </Alert>
+        </motion.div>
+      )}
+
+      {/* Dialog nhập OTP */}
+      <Dialog
+        open={showOtpDialog}
+        onClose={handleCloseOtpDialog}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Email confirmation</DialogTitle>
+        <DialogContent>
+          <p className="mt-3" style={{ fontSize: "16px", fontWeight: "600" }}>
+            Enter code
+          </p>
+          <MuiOtpInput length={6} value={""} onChange={""} />
+          <Box>
+            <div className="flex flex-row justify-between">
+              <div className="">
+                <Button
+                  type="cancel"
+                  variant="contained"
+                  sx={{ mt: 2, mr: 2 }}
+                  style={{
+                    backgroundColor: "#F85F60",
+                    width: "100px",
+                    height: "40px",
+                  }}
+                  onClick={"handleResendOtp"}
+                >
+                  Resend
+                </Button>
+              </div>
+            </div>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleCloseOtpDialog}
+            style={{
+              color: "#F85F60",
+              width: "100px",
+              height: "40px",
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            style={{
+              marginRight: "16px",
+              backgroundColor: "#3867A5",
+              width: "100px",
+              height: "40px",
+            }}
+            onClick={handleOtpVerification}
+          >
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* Dialog nhập mật khẩu mới */}
+      <Dialog
+        open={showPasswordDialog}
+        onClose={() => setShowPasswordDialog(false)}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Change Password</DialogTitle>
+        <DialogContent>
+          <TextField
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="New Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            label="Confirm Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => setShowPasswordDialog(false)}
+            style={{
+              color: "#F85F60",
+              width: "100px",
+              height: "40px",
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={handleChangePasswordFormSubmit}
+            variant="contained"
+            style={{
+              marginRight: "16px",
+              backgroundColor: "#3867A5",
+              width: "100px",
+              height: "40px",
+            }}
+          >
+            Done
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
