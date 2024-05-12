75f890ad7b4009311a634dccddd1e6a444eb051b
dangkhoabo2002 <dangkhoabo2002@gmail.com>
2024-05-12 14:24:57 +0700

    save

--- src/screens/userSubscribePayment.js
+++ src/screens/userSubscribePayment.js
@@ -5,6 +5,7 @@ import QR from "../assets/QRcode.png";
 import Button from "@mui/material/Button";
 
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function UserSubscribePayment() {
   const [isPaid, setIsPaid] = useState(true);
@@ -13,6 +14,8 @@ export default function UserSubscribePayment() {
     setIsPaid(!isPaid);
   };
 
+  const { resultCode, orderId, amount } = useParams();
+  console.log(resultCode);
   return (
     <div className="backgroundContainer">
       <div className="bodyContainer flex flex-col justify-center items-center py-12">
@@ -23,33 +26,21 @@ export default function UserSubscribePayment() {
         <h2 className="text-lg font-bold pb-4">
           Transaction code: 2625348113652523008
         </h2>
-        {isPaid && (
-          <>
-            <img  loading="lazy" className="w-60" src={QR} alt="QRcode" />
-            <h2 className="font-bold py-6">
-              Scan the QR code with the MOMO app to pay
-            </h2>
-          </>
-        )}
-        {!isPaid && (
-          <>
-            <h2 className="font-bold py-6 text-[#37E030] text-2xl uppercase">
-              Successful transaction
-            </h2>
-            <h3 className="font-bold">
-              You are using Package 1, your privileges when using this package
-              are:
-            </h3>
-            <div>Package</div>
-          </>
-        )}
-
+        <>
+          <h2 className="font-bold py-6 text-[#37E030] text-2xl uppercase">
+            Successful transaction
+          </h2>
+          <h3 className="font-bold">
+            You are using Package 1, your privileges when using this package
+            are:
+          </h3>
+          <div>Package</div>
+        </>
         <Button
           sx={{ padding: "8px 42px", color: "white" }}
           variant="contained"
-          onClick={handleStatusPayment}
         >
-          Check transaction results
+          Back to Dashboard
         </Button>
       </div>
     </div>
