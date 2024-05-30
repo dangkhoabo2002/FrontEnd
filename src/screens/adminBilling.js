ebb0b14246a45c2c6734561166cd4b3f59d490f9
dangkhoabo2002 <dangkhoabo2002@gmail.com>
2024-05-30 15:59:10 +0700

    deploy: 2nd deployment after fixing

--- src/screens/adminBilling.js
+++ src/screens/adminBilling.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import SidebarAdmin from "../components/sidebarAdmin";
 import NavigationAdmin from "../components/navAdmin";
 import "../css/adminBilling.css";
-import { Button } from "@mui/material";
+import { Box, Button, Divider, Typography } from "@mui/material";
 import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
@@ -14,7 +14,7 @@ export default function AdminBillings() {
   const [billingInfo, setBillingInfo] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
-
+  console.log(billingInfo);
   const handleViewBilling = (id) => {
     handleGetBilling(id);
   };
@@ -200,7 +200,7 @@ export default function AdminBillings() {
               {currentItems?.map((bill) => (
                 <tr key={bill.billing_id} className="border-t">
                   <td className="p-4">{bill.timestamp}</td>
-                  <td className="p-4">{bill.amount} VND</td>
+                  <td className="p-4">{bill.amount} VNĐ</td>
                   <td className="p-4">
                     <div
                       className="text-white text-center rounded-full px-3 py-1 text-sm"
@@ -245,42 +245,95 @@ export default function AdminBillings() {
             />
           </div>
           <div id="popup1" className="overlay">
-            <div className="popup">
-              <div className="popup_content">
-                <h1>Billing Information</h1>
-                <div className="flex flex-row justify-start">
-                  <div className="flex flex-col ">
-                    <h2>Transaction fee:</h2>
-                    <h2>Billing Id:</h2>
-                    <h2>Status</h2>
-                    <h2>Customer Id:</h2>
-                    <h2>Date:</h2>
-                    <h2>Subscription Id:</h2>
-                  </div>
-                  <div className="flex flex-col">
-                    <h2>{billingInfo?.amount}đ</h2>
-                    <h2>{billingInfo?.billing_id}</h2>
-                    <h2>{billingInfo?.billing_status}</h2>
-                    <h2>{billingInfo?.customer_id}</h2>
-                    <h2>{billingInfo?.timestamp}</h2>
-                    <h2>{billingInfo?.subscription_id}</h2>
-                  </div>
-                </div>
-                <div className="popup_btn">
-                  <a id="popup_btn" href="#" className="close">
-                    <div
-                      style={{
-                        fontSize: "16px",
-                        fontWeight: "Bold",
-                        letterSpacing: "0.09rem",
+            <Box
+              className="popup"
+              sx={{ width: { xs: "90%", md: "40%" }, padding: 4 }}
+            >
+              <Box className="popup_content">
+                <Typography
+                  variant="h4"
+                  component="h1"
+                  sx={{
+                    fontWeight: "bold",
+                    color: "#637381",
+                    borderBottom: "2px solid #637381",
+                    paddingBottom: 2,
+                    marginBottom: 4,
+                  }}
+                >
+                  Billing Information
+                </Typography>
+                <Box className="billingInfo">
+                  <Box className="leftInfo">
+                    <Typography variant="h6">Transaction fee:</Typography>
+                    <Typography variant="h6">Billing Id:</Typography>
+                    <Typography variant="h6">Status:</Typography>
+                    <Typography variant="h6">Customer Id:</Typography>
+                    <Typography variant="h6">Date:</Typography>
+                    <Typography variant="h6">Time Zone:</Typography>
+                    <Typography variant="h6">Subscription Id:</Typography>
+                  </Box>
+                  <Box className="rightInfo ">
+                    <Typography variant="h6">
+                      {billingInfo?.amount} VNĐ
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        width: "220px",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
                       }}
                     >
-                      Close
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
+                      {billingInfo?.billing_id}
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      className={
+                        billingInfo?.billing_status === "paid"
+                          ? "text-green-500 font-bold"
+                          : billingInfo?.billing_status === "pending"
+                          ? "text-yellow-500 font-bold"
+                          : "text-red-500 font-bold"
+                      }
+                    >
+                      {billingInfo?.billing_status}
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        width: "220px",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
+                    >
+                      {billingInfo?.customer_id}
+                    </Typography>
+                    <Typography variant="h6">
+                      {billingInfo?.timestamp}
+                    </Typography>
+                    <Typography variant="h6">
+                      {billingInfo?.subscription_id}
+                    </Typography>
+                  </Box>
+                </Box>
+                <Divider sx={{ marginY: 4 }} />
+                <Box
+                  className="popup_btn"
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    className="close"
+                    sx={{ borderRadius: 2, paddingX: 4 }}
+                    href="#"
+                  >
+                    Close
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
           </div>
         </div>
       </div>
